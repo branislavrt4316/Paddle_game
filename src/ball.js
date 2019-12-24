cd484c6ab3756a671004951ed3b7b82396574526
Branislav Vujanovic <branislavvujanovic4@gmail.com>
2019-12-24 22:04:05 +0100

    Collision Detection between the Ball and Paddle

--- src/ball.js
+++ src/ball.js
@@ -5,8 +5,10 @@ export default class Ball {
     this.gameWidth = game.gameWidth; // Width of canvas
     this.gameHeight = game.gameHeight; // Height of canvas
 
+    this.game = game;
+
     this.position = { x: 10, y: 10 }; // Start position for the Ball
-    this.speed = { x: 4, y: 2 }; // Ball speed
+    this.speed = { x: 6, y: 3 }; // Ball speed
     this.size = 36; // Ball size
   }
 
@@ -26,13 +28,32 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
-    //keep Ball inside of canvas
+    //wall on left or right
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
 
+    //wall on top of bottom
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+
+    //check collision with paddle
+
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPaddle = this.game.paddle.position.y;
+
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rightSideOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfPaddle &&
+      this.position.x >= leftSideOfPaddle &&
+      this.position.x + this.size <= rightSideOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
